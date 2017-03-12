8b5c2c8b1853e0fab54b0d20332f7b858df733a0
nicepersonniceperson <nicepersonniceperson@users.noreply.github.com>
2017-03-13 00:15:01 +0200

    Create wordtonumber.cpp

---
+++ wordtonumber.cpp
@@ -0,0 +1,122 @@
+#include "iostream"
+#include "string"
+#include "fstream"
+
+using namespace std;
+
+int main(){
+string word;
+ifstream ifile;
+ifstream rofile;
+ofstream wofile("number.txt", ios_base::app);
+ofstream efile("wordn.txt");
+rofile.open ("number.txt");
+ifile.open ("word.txt");
+int x=0;
+while (rofile >> word) x++;
+if (x>256)
+{
+        cout << "أقصى عدد للكلمات 256";
+        exit(1);
+}
+string oarray[x];
+x=0;
+rofile.clear();
+rofile.seekg(0, rofile.beg);
+while (rofile >> word)
+{
+        oarray[x]=word;
+        x++;
+}
+rofile.clear();
+rofile.seekg(0, rofile.beg);
+x=0;
+int found;
+while (ifile >> word)
+{
+        found=0;
+        for( int a = 0; a < sizeof(oarray)/sizeof(oarray[0]); a = a + 1 )
+        {
+                if ( word == oarray[a] )
+                {
+                        found=1;
+                        break;
+                }
+        }
+        if (found == 0)
+        {
+                x++;
+        }
+}
+cout << x << ": عدد الكلمات الجديدة";
+if (x>256-sizeof(oarray))
+{
+        cout << "أقصى عدد للكلمات 256";
+        exit(1);
+}
+
+cout << x << ": عدد الكلمات الجديدة";
+if (x>256-sizeof(oarray))
+{
+        cout << "أقصى عدد للكلمات 256";
+        exit(1);
+}
+
+string noarray[x];
+
+ifile.clear();
+ifile.seekg(0, ifile.beg);
+x=0;
+found=0;
+while (ifile >> word)
+{
+        found=0;
+        for( int a = 0; a < sizeof(oarray)/sizeof(oarray[0]); a = a + 1 )
+        {
+                if ( word == oarray[a] || word == "" )
+                {
+                        found=1;
+                        break;
+                }
+        }
+        if (found == 0)
+        {
+                noarray[x]=word;
+                x++;
+        }
+}
+
+ifile.clear();
+ifile.seekg(0, ifile.beg);
+
+for( int a = 0; a < sizeof(noarray)/sizeof(noarray[0]); a = a + 1 )
+{
+        wofile << noarray[a] << "\n";
+}
+
+char c;
+
+ifile.clear();
+ifile.seekg(0, ifile.beg);
+while (ifile >> word)
+{
+        for( int a = 0; a < sizeof(oarray)/sizeof(oarray[0]); a = a + 1 )
+        {
+                if ( word == oarray[a] || word == "" )
+                {
+                        c = 'A' - 56 + a;
+                        efile << c;
+                        break;
+                }
+        }
+        for( int a = 0; a < sizeof(noarray)/sizeof(noarray[0]); a = a + 1 )
+        {
+                if ( word == noarray[a] )
+                {
+                        c = 'A' - 56 + a +sizeof(oarray);
+                        efile << c;
+                        break;
+                }
+        }
+}
+}
