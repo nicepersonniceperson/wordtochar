a6f94d03622137bb6c221a16deece8355c1380cf
nicepersonniceperson <nicepersonniceperson@users.noreply.github.com>
2017-03-13 00:07:26 +0200

    Create new file

---
+++ numbertoword.cpp
@@ -0,0 +1,44 @@
+#include "iostream"
+#include "string"
+#include "fstream"
+
+using namespace std;
+
+int main(){
+string word;
+ifstream ifile;
+ifstream rofile;
+ofstream wofile("number.txt", ios_base::app);
+ofstream efile("wordx.txt");
+rofile.open ("number.txt");
+ifile.open ("wordn.txt");
+int x=0;
+while (rofile >> word) x++;
+if (x>256)
+{
+        cout << "big file";
+        exit(1);
+}
+string oarray[x];
+x=0;
+rofile.clear();
+rofile.seekg(0, rofile.beg);
+while (rofile >> word)
+{
+        oarray[x]=word;
+        x++;
+}
+char c;
+char character;
+while (ifile >> noskipws >> character)
+{
+        for( int a = 0; a < sizeof(oarray)/sizeof(oarray[0]); a = a + 1 )
+        {
+                if ( character == a )
+                {
+                        cout << oarray[a] << " ";
+                        break;
+                }
+        }
+}
+}
